bd162275d1a1886d2f4cdde9e71af4bc2e719a08
Alper Oren <alper.oren@hotmail.co.uk>
2020-05-28 15:32:45 +0100

    test

--- Class1.cs
+++ Class1.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit;
 
 namespace UnitTests
 {
     public class Class1
     {
+        [Fact]
+        public void Hello()
+        {
+        }
     }
 }
