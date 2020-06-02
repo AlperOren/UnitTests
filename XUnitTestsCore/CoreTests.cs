1460c6fb1602679db2e6824c5146e740651fe549
Alper Oren <alper.oren@hotmail.co.uk>
2020-06-02 10:46:17 +0100

    add few test projects

---
+++ XUnitTestsCore/CoreTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace XUnitTestsCore
+{
+    public class CoreTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ XUnitTestsStandard/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace XUnitTestsStandard
+{
+    public class Class1
+    {
+    }
+}
