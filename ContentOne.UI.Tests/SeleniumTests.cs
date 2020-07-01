69e951681f8e3f89a3e32f9b54068d58643ebfb2
alperoren <alper.oren@hotmail.co.uk>
2020-07-01 08:08:57 +0100

    Selenium

---
+++ ContentOne.UI.Tests/SeleniumTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace ContentOne.UI.Tests
+{
+    public class SeleniumTests
+    {
+        [Trait("Category", "Smoke")]
+        [Fact]
+        public void LoadUmbraco()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:2728/umbraco");
+            }
+        }
+    }
+}
